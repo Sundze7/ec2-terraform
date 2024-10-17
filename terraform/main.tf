1be7f02834daec463f469e2dddd57703aa1cfacc
Sundze <keyonmwesundze@mail.com>
2024-10-17 02:10:32 +0100

    debugging deployment stage

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.medium"
   key_name               = "stellahtech"
   vpc_security_group_ids = [aws_security_group.new-sg.id]
+  associate_public_ip_address = true
   # user_data              = templatefile("./install.sh", {})
   user_data = <<-EOT
     #!/bin/bash
